0bc250d14f077fe2139faed7684a154aeea9d08a
shatakshi_sslab <tiwari114khushi@gmail.com>
2023-08-23 22:50:31 +0530

    program 6

---
+++ program6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{	
+	char buf[100];
+	int char_read = read(STDIN_FILENO, buf, 100);
+	write(STDOUT_FILENO , buf, char_read );
+	return 0 ;
+}
