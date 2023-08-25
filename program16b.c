7d862f476f061c82c52f2d06963ea789bb72d157
shatakshi_sslab <tiwari114khushi@gmail.com>
2023-08-25 23:46:57 +0530

    program 16 read lock

---
+++ program16b.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+int main(void)
+{
+        struct flock lock;
+        int fd;
+        fd = open("db" , O_RDWR );
+        lock.l_type = F_RDLCK ;
+        lock.l_whence = SEEK_SET ;
+lock.l_start = 0;
+lock.l_len= 0;
+lock.l_pid= getpid() ;
+printf("before entering into critical section \n ");
+fcntl(fd , F_SETLKW , &lock );
+printf("inside the critical section .. \n");
+printf("enter to unlock ...\n");
+getchar();
+printf("unlocked \n");
+lock.l_type = F_UNLCK;
+fcntl(fd, F_SETLK , &lock);
+printf("excited critical section \n");
+
+}
+
+
